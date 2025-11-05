e23ed008eab2ecfd420dd15f50a6f39a4e639e04
p.panahpour <p.panahpour@WIN02964>
2025-11-05 12:03:39 +0330

    end of Statics component

---
+++ src/JsonViewerComponent/Models/JsonStats.cs
@@ -0,0 +1,23 @@
+﻿namespace JsonViewerComponent.Models;
+
+public class JsonStats
+{
+    public int TotalProperties { get; set; }
+    public int ObjectCount { get; set; }
+    public int ArrayCount { get; set; }
+    public int ArrayItems { get; set; }
+    public int StringCount { get; set; }
+    public int NumberCount { get; set; }
+    public int BooleanCount { get; set; }
+    public int NullCount { get; set; }
+    public int MaxDepth { get; set; }
+    public long RawSize { get; set; }
+    public long FormattedSize { get; set; }
+    public double AvgPropertiesPerObject { get; set; }
+    public double AvgArrayLength { get; set; }
+    public int TotalStringLength { get; set; }
+    public int MaxStringLength { get; set; }
+    public int TotalNumberLength { get; set; }
+    public Dictionary<string, int> PropertyCounts { get; set; } = new Dictionary<string, int>();
+
+}
---
+++ src/JsonViewerComponent/Models/SearchResult.cs
@@ -0,0 +1,10 @@
+﻿namespace JsonViewerComponent.Models;
+
+public class SearchResult
+{
+    public int MatchCount { get; set; }
+    public int CurrentMatchIndex { get; set; }
+    public bool HasMatches => MatchCount > 0;
+    public bool CanNavigateNext => CurrentMatchIndex < MatchCount - 1;
+    public bool CanNavigatePrevious => CurrentMatchIndex > 0;
+}
